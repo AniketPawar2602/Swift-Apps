469ebf2570d70841455d4a091acd1196d703363c
Aniket Satish Pawar <105710680+AniketPawar2602@users.noreply.github.com>
2024-01-05 07:51:28 +0530

    Add files via upload

---
+++ BotResponse.swift
@@ -0,0 +1,25 @@
+
+//
+//  BotResponse.swift
+//  ISE Chatbot
+//
+//  Created by AL19 on 04/12/23.
+//
+
+import Foundation
+func getBotResponse(message : String) -> String {
+    let tempMessage=message.lowercased()
+    
+    if tempMessage.contains("hello"){
+        return "Hey There!"
+    }
+    else if tempMessage.contains("goodbye"){
+        return "Talk to you later!"
+    }
+    else if tempMessage.contains("how are you"){
+        return "I'm Fine"
+    }
+    else{
+        return "That's Cool"
+    }
+}
---
+++ Quiz.swift
@@ -0,0 +1,13 @@
+//
+//  Quiz.swift
+//  Quiz Apps
+//
+//  Created by AL19 on 29/12/23.
+//
+
+import Foundation
+struct Question {
+    let text: String
+    let options: [String]
+    let correctAnswerIndex: Int
+}
